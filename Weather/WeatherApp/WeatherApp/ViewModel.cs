b0172139ded227788025bc51dec2ef630e57c9c2
Stef-R <username@user.noreply.github.com>
2016-12-12 15:26:01 +0100

    add Start brug af viewmodel

---
+++ Weather/WeatherApp/WeatherApp/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp;
+
+namespace WeatherApp
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private DataService model;
+        private Weather currentWeather;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(DataService model)
+        {
+            this.model = model;
+        }
+
+        public Weather CurrentWeather
+        {
+            get
+            {
+                return currentWeather;
+            }
+            private set
+            {
+                currentWeather = value;
+                NotifyPropertyChanged("CurrentWether");
+            }
+        }
+        private void NotifyPropertyChanged(string property)
+        {
+            var propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
